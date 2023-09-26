01662e9966aecc2384f4d61251d31fae1ceb95f9
Owner129 <niko2yurii@gmail.com>
2023-09-26 13:43:26 +0300

    Update .gitignore, add new folders

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,48 @@ const uglify = require('gulp-uglify');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
 const cssnano = require('gulp-cssnano');
+const browser_sync = require("browser-sync").create();
+const debug = require("gulp-debug");
+const opt = { title: "log", showCount: false };
 
+function browserSync() {
+  browser_sync.init({                // Ініціалізація browser_sync
+      server: { baseDir: "build/" }, // Встановлюємо базову директорію
+      notify: false,                 // Вимикаємо інформаційні сповіщення
+      online: true                   /* Дозволяємо підключення пристроїв через 
+                                        локальну мережу (напр. смартфонів, планшетів і т.д.) */
+  })
+}
 gulp.task("html", function () {
     return gulp.src("app/*.html").pipe(gulp.dest("dist"));
-  });
+});
   
-gulp.task("sass", function () {
-    return gulp.src("app/sass/*.sass")
+gulp.task("scss", function () {
+    return gulp.src("app/sass/*.scss")
     .pipe(concat("styles.scss"))
     .pipe(autoprefixer({ overrideBrowserslist: ["last 2 versions"], cascade: false,}))
     .pipe(cssnano())
     .pipe(rename({ suffix: ".min" }))
-    .pipe(gulp.dest("dist/css"));
-  });
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browser_sync.stream());
+});
 gulp.task("scripts", function () {
-    return gulp
-    .src("app/js/*.js")
-    .pipe(concat("script.js"))
-    .pipe(uglify())
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(gulp.dest("dist/js"));
-  });
+  return gulp
+  .src("app/js/*.js")
+  .pipe(concat("script.js"))
+  .pipe(uglify())
+  .pipe(rename({ suffix: ".min" }))
+  .pipe(gulp.dest("dist/js"))
+  .pipe(browser_sync.stream());
+});
+gulp.task('imgs', function (){
+  return gulp
+  .src("app/img")
+
+});
 gulp.task("watch", function () {
     gulp.watch("app/*.html", gulp.series("html"));
     gulp.watch("app/js/*.js", gulp.series("scripts"));
     gulp.watch("app/sass/*.sass", gulp.series("sass"));
-    //gulp.watch("app/img/*.{jpg,jpeg,png,gif}", gulp.series("imgs"));
+    gulp.watch("app/img/*.{jpg,jpeg,png,gif}", gulp.series("imgs"));
   });
